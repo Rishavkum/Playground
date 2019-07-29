af6ae068a7d8a8c35db1759e347e5d0d26fd88ab
Rishavkum <46356623+Rishavkum@users.noreply.github.com>
2019-07-29 08:16:45 -0700

    Test code pushed to repository.

---
+++ Extract first digit of a three digit number/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+      int num=sc.nextInt();
+      int firstdigit=num/100;
+      System.out.println(firstdigit);// Type your code here
+	}
+}
